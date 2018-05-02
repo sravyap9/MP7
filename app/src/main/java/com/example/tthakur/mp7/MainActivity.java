4bc4e793fd9f2da3f6813dc6eb676d16be59efbf
Tanmay Thakur <tthakur2@illinois.edu>
2018-05-01 22:07:52 -0500

    Creating CheckWin

--- app/src/main/java/com/example/tthakur/mp7/MainActivity.java
+++ app/src/main/java/com/example/tthakur/mp7/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -13,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     boolean gameOver;
     boolean playerOneTurn;
     boolean isDraw;
+
+    TextView playerOneScoreText;
+    TextView playerTwoScoreText;
 
     Button topLeft;
     Button topCenter;
@@ -28,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
 
     Button newGame;
 
+    int playerOneScore = 0;
+    int playerTwoScore = 0;
+
     ArrayList<Button> buttonList;
 
     char board[][] = new char[3][3];
@@ -36,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerOneScoreText = findViewById(R.id.playerOneTB);
+        playerTwoScoreText = findViewById(R.id.playerTwoTB);
 
         topLeft = findViewById(R.id.topLeft);
         topCenter = findViewById(R.id.topCenter);
@@ -208,19 +218,96 @@ public class MainActivity extends AppCompatActivity {
     {
         if(!gameOver)
         {
-            if (playerOneTurn)
-            {
-                Toast.makeText(getApplicationContext(), "Player Two Turn", Toast.LENGTH_SHORT).show();
-                clicked.setText("X");
-                playerOneTurn = false;
-            }
+            
+        }
+    }
 
-            else
+    public String checkWin()
+    {
+        //Horizontals
+        if ((topLeft.getText() == topCenter.getText()) &&
+                (topCenter.getText() == topRight.getText()) &&
+                (!isEmpty(topLeft, topCenter, topRight)))
+        {
+            return "win";
+        }
+        else if ((middleLeft.getText() == middleCenter.getText())
+                && (middleCenter.getText() == middleRight.getText()) &&
+                (!isEmpty(middleLeft, middleCenter, middleRight)))
+        {
+            return "win";
+        }
+        else if ((bottomLeft.getText() == bottomCenter.getText()) &&
+                (bottomCenter.getText() == bottomRight.getText()) &&
+                (!isEmpty(bottomLeft, bottomCenter, bottomRight)))
+        {
+            return "win";
+        }
+
+        //Verticals
+        else if ((topLeft.getText() == middleLeft.getText()) &&
+                (middleLeft.getText() == bottomLeft.getText()) &&
+                (!isEmpty(topLeft, middleLeft, bottomLeft)))
+        {
+            return "win";
+        }
+        else if ((topCenter.getText() == middleCenter.getText()) &&
+                (middleCenter.getText() == bottomCenter.getText()) &&
+                (!isEmpty(topCenter, middleCenter, bottomCenter)))
+        {
+            return "win";
+        }
+        else if ((topRight.getText() == middleRight.getText()) &&
+                (middleRight.getText() == bottomRight.getText()) &&
+                (!isEmpty(topRight, middleRight, bottomRight)))
+        {
+            return "win";
+        }
+
+        //Diagonals
+        else if ((topLeft.getText() == middleCenter.getText()) &&
+                (middleCenter.getText() == bottomRight.getText()) &&
+                (!isEmpty(topLeft, middleCenter, bottomRight)))
+        {
+            return "win";
+        }
+        else if ((topRight.getText() == middleCenter.getText()) &&
+                (middleCenter.getText() == bottomLeft.getText()) &&
+                (!isEmpty(topRight, middleCenter, bottomLeft)))
+        {
+            return "win";
+        }
+
+        //draw
+        else if (checkDraw())
+        {
+            return "draw";
+        }
+
+        return "noWin";
+
+    }
+
+    public boolean isEmpty(Button first, Button second, Button third)
+    {
+        if (first.getText() != " " && second.getText() != " " && third.getText() != " ")
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean checkDraw()
+    {
+        for (int i = 0; i < buttonList.size(); i++)
+        {
+            if (buttonList.get(i).getText() == " ")
             {
-                Toast.makeText(getApplicationContext(), "Player One Turn", Toast.LENGTH_SHORT).show();
-                clicked.setText("O");
-                playerOneTurn = true;
+                return false;
             }
         }
+
+        return true;
     }
 }
